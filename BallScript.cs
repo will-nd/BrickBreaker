3bb7c91cbd2a24eadc31f75b83aac9fb80fe7c3a
William Ndudirim <will89.wn@googlemail.com>
2022-09-14 15:01:59 +0100

    Add files via upload

---
+++ BallScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour{
+
+    public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float speed;
+    public Transform explosion;
+    public GameManager gm;
+    public Transform powerUp;
+
+    // Start is called before the first frame update
+    void Start() {
+      rb = GetComponent<Rigidbody2D> ();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+      if (gm.gameOver){
+        return;
+      }
+
+      if(!inPlay) {
+        transform.position = paddle.position;
+      }
+
+      if(Input.GetButtonDown ("Jump") && !inPlay) {
+        inPlay = true;
+        rb.AddForce (Vector2.up * speed);
+      }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+      if(other.CompareTag ("bottom")) {
+        rb.velocity = Vector2.zero;
+        inPlay = false;
+        gm.UpdateLives(-1);
+      }
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+      if(other.transform.CompareTag ("brick")) {
+        BrickScript brickScript = other.gameObject.GetComponent<BrickScript>();
+        if (brickScript.hitsToBreak > 1){
+          brickScript.BreakBrick();
+        } else{
+              int randChance = Random.Range(1, 101);
+              if (randChance < 16){
+                Instantiate(powerUp, other.transform.position, other.transform.rotation);
+              }
+
+              Transform newExplosion = Instantiate(explosion, other.transform.position, other.transform.rotation);
+              Destroy (newExplosion.gameObject, 2.5f);
+
+              gm.UpdateScore(brickScript.points);
+              gm.UpdateNumberOfBricks();
+              Destroy (other.gameObject);
+          }
+      }
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+  public int lives;
+  public int score;
+  public Text livesText;
+  public Text scoreText;
+  public bool gameOver;
+  public GameObject gameOverPanel;
+  public GameObject loadLevelPanel;
+  public int numberOfBricks;
+  public Transform[] levels;
+  public int currentLevelIndex = 0;
+
+    // Start is called before the first frame update
+    void Start(){
+      livesText.text = "Lives: " + lives;
+      scoreText.text = "Score: " + score;
+      numberOfBricks = GameObject.FindGameObjectsWithTag("brick").Length;
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+
+    public void UpdateLives(int changeInLives) {
+      lives += changeInLives;
+
+      //check for no lives left and trigger end of game
+      if(lives <= 0){
+        lives = 0;
+        GameOver();
+      }
+      livesText.text = "Lives: " + lives;
+    }
+
+    public void UpdateScore(int points){
+      score += points;
+
+      scoreText.text = "Score: " + score;
+    }
+
+    public void UpdateNumberOfBricks(){
+      numberOfBricks--;
+      if (numberOfBricks <= 0){
+        if (currentLevelIndex >= levels.Length - 1){
+          GameOver();
+        } else {
+          loadLevelPanel.SetActive(true);
+          gameOver = true;
+          Invoke("LoadLevel", 4f);
+        }
+      }
+    }
+
+    void LoadLevel() {
+      currentLevelIndex++;
+      Instantiate(levels[currentLevelIndex], Vector2.zero, Quaternion.identity);
+      numberOfBricks = GameObject.FindGameObjectsWithTag("brick").Length;
+      gameOver = false;
+      loadLevelPanel.SetActive(false);
+    }
+
+    void GameOver(){
+      gameOver = true;
+      gameOverPanel.SetActive(true);
+    }
+
+    public void PlayAgain(){
+      SceneManager.LoadScene("BrickBreakerGame");
+    }
+
+    public void Quit(){
+      Application.Quit();
+      Debug.Log("Game Quit");
+    }
+}
